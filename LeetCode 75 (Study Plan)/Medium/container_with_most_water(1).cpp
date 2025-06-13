90b9cd4c2c80b36651c648fab7943c7a1c8a4e6e
Arwa Abbas <arwaabbaskerani@gmail.com>
2025-06-13 10:35:37 +0500

    Create container_with_most_water(1).cpp

---
+++ LeetCode 75 (Study Plan)/Medium/container_with_most_water(1).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+      int maxarea=0;
+      for (int i=0;i<height.size();i++)
+      {
+        for (int j=i+1;j<height.size();j++)
+        {
+        int length=min(height[i],height[j]);
+        int breath=j-i;
+        int area=length*breath;
+        if (maxarea<area)
+        maxarea=area;
+        }     
+      }
+      return maxarea;
+    }
+};
+
+//NOTE: this only passes 58/65 test cases, time limit exceeded.
