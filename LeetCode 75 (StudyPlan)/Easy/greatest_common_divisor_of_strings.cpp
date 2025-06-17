1a9fc5c237e3c4379124a389a2a856059fcec935
Arwa Abbas <arwaabbaskerani@gmail.com>
2025-06-17 17:59:59 +0500

    Update greatest_common_divisor_of_strings.cpp

--- LeetCode 75 (StudyPlan)/Easy/greatest_common_divisor_of_strings.cpp
+++ LeetCode 75 (StudyPlan)/Easy/greatest_common_divisor_of_strings.cpp
@@ -10,3 +10,15 @@ public:
         return "";
     }
 };
+
+/* Explanation: 
+In this problem, basically there a big hint called gcd. Instead of loop iteration and checking one character 
+by character for str1 and str2 we instead use gcd to find length of output. The iteration method wont work for
+test case 2 as pattern repeats in str1. We extract the sub string from str1 by finding gcd which will be 
+indicate the no of characters needed to be extracted. 
+To handle test case 3 we check if (str1+str2) is not equal to (str2+str1) eg LEETCODE, CODELEET. 
+For other cases, ABCABCABC=ABCABCABC etc.
+
+Time Complexity: For comparsion (str1+str2==str2+str1) -> O(n+m) : where n is length of str1 and m is length of str2.
+Space Complexity: O(n+m) -> considering both input strings take memory.
+*/
